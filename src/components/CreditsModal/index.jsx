69db83f071d54f498be20ae4a24528241fc9f712
Philip Karpiak <philip@eswat.ca>
2016-11-22 08:12:01 -0500

    Update Credits with props to @osavox

--- src/components/CreditsModal/index.jsx
+++ src/components/CreditsModal/index.jsx
@@ -5,7 +5,8 @@ const CreditsModal = (props) => {
   return (
     <div>
       <p>Created by <a className={styles.link} href="https://eswat.ca/">Philip Karpiak</a></p>
-      <p>Icons by <a className={styles.link} href="http://game-icons.net/">Game-icons.net</a></p>
+      <p>Logo by <a className={styles.link} href="https://twitter.com/osavox">osavox</a></p>
+      <p>Item icons by <a className={styles.link} href="http://game-icons.net/">Game-icons.net</a></p>
     </div>
   );
 };
